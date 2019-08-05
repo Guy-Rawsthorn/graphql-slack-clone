67cbbae5e441102af5d49a9a56428c707edddfab
Guy Rawsthorn <guyrawsthorn@Guys-MacBook-Pro.local>
2019-08-05 23:38:07 +0100

    Login and Reigster form client validation.

--- slack-clone-client/src/Routes/login.js
+++ slack-clone-client/src/Routes/login.js
@@ -1,64 +1,113 @@
 import React, { Component } from "react";
 import { extendObservable } from "mobx";
 import { observer } from "mobx-react";
-import { Container, Header, Input, Button } from "semantic-ui-react";
-import {loginMutation} from '../Queries/users';
-import {graphql} from 'react-apollo';
+import {
+  Message,
+  Container,
+  Header,
+  Input,
+  Button,
+  Form,
+  Checkbox
+} from "semantic-ui-react";
+import { loginMutation } from "../Queries/users";
+import { graphql } from "react-apollo";
 
-  class Login extends Component {
-    constructor(props) {
-      super(props);
+class Login extends Component {
+  constructor(props) {
+    super(props);
 
-      extendObservable(this, {
-        email: "",
-        password: ""
+    extendObservable(this, {
+      email: "",
+      password: "",
+      errors: {}
+    });
+  }
+
+  onSubmit = async () => {
+    const { email, password } = this;
+    console.log(email, password);
+    const response = await this.props.mutate({
+      variables: { email, password }
+    });
+    console.log(response);
+    const { ok, token, refreshToken, errors } = response.data.login;
+
+    if (ok) {
+      localStorage.setItem("token", token);
+      localStorage.setItem("refreshToken", refreshToken);
+      this.props.history.push("/");
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
       });
-    }
-
-    onSubmit = async () => {
-      const { email, password } = this;
-      console.log(email, password);
-      const response = await this.props.mutate({
-        variables: {email, password}
-      }) 
-      console.log(response)
-      const {ok, token, refreshToken} = response.data.login
-
-      if (ok) {
-        localStorage.setItem('token', token)
-        localStorage.setItem('refreshToken', refreshToken)
-      }
-    };
-
-    onChange = e => {
-      const { name, value } = e.target;
-      this[name] = value;
-    };
-    render() {
-      const { email, password } = this;
-
-      return (
-        <Container text>
-          <Header as="h2">Login</Header>
-          <Input
-            placeholder="Email"
-            name="email"
-            onChange={this.onChange}
-            value={email}
-            fluid
-          />
-          <Input
-            placeholder="Password"
-            name="password"
-            onChange={this.onChange}
-            value={password}
-            type="password"
-            fluid
-          />
-          <Button onClick={this.onSubmit}>Register</Button>
-        </Container>
-      );
+      this.errors = err;
     }
   };
 
-  export default graphql(loginMutation)(observer(Login));
+  onChange = e => {
+    const { name, value } = e.target;
+    this[name] = value;
+  };
+  render() {
+    const {
+      email,
+      password,
+      errors: { emailError, passwordError }
+    } = this;
+
+    const errorList = [];
+
+    if (emailError) {
+      errorList.push(emailError);
+    }
+
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
+
+    return (
+      <Container text>
+        <Form>
+          <Header as="h2">Login</Header>
+
+          <label>Email</label>
+          <Form.Field error={!!emailError}>
+            <Input
+              placeholder="Email"
+              name="email"
+              onChange={this.onChange}
+              value={email}
+              fluid
+            />
+          </Form.Field>
+
+          <label>Password</label>
+          <Form.Field error={!!passwordError}>
+            <Input
+              placeholder="Password"
+              name="password"
+              onChange={this.onChange}
+              value={password}
+              type="password"
+              fluid
+            />
+          </Form.Field>
+
+          <Button onClick={this.onSubmit}>Login</Button>
+        </Form>
+
+        {errorList.length ? (
+          <Message
+            error
+            header="There was some errors with your submission"
+            list={errorList}
+          />
+        ) : null}
+      </Container>
+    );
+  }
+}
+
+export default graphql(loginMutation)(observer(Login));
--- slack-clone-client/src/Routes/register.js
+++ slack-clone-client/src/Routes/register.js
@@ -1,54 +1,68 @@
 import React, { Component } from "react";
-import { Message, Container, Header, Input, Button } from "semantic-ui-react";
-import { registerMutation } from '../Queries/users';
-import { graphql } from 'react-apollo';
+import {
+  Form,
+  Message,
+  Container,
+  Header,
+  Input,
+  Button
+} from "semantic-ui-react";
+import { registerMutation } from "../Queries/users";
+import { graphql } from "react-apollo";
 
 class Register extends Component {
   state = {
     username: "",
-    usernameError: '',
+    usernameError: "",
     email: "",
-    emailError: '',
+    emailError: "",
     password: "",
-    passwordError: ''
+    passwordError: ""
   };
 
   onChange = e => {
-      const {name, value} = e.target;
-      this.setState({
-          [name]:value
-      })
-  }
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
   onSubmit = async () => {
     this.setState({
-      usernameError: '',
-      emailError: '',
-      passwordError: '',
+      usernameError: "",
+      emailError: "",
+      passwordError: ""
     });
 
-      const {username, email, password} = this.state
-      const response = await this.props.mutate({
-          variables: {username, email, password},
+    const { username, email, password } = this.state;
+    const response = await this.props.mutate({
+      variables: { username, email, password }
+    });
+
+    const { ok, errors } = response.data.register;
+
+    if (ok) {
+      this.props.history.push("/");
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
       });
+      this.setState(err);
+    }
 
-      const {ok, errors} = response.data.register;
-
-      if (ok) {
-          this.props.history.push('/')
-      } else {
-          const err = {}
-          errors.forEach(({path, message}) => {
-              err[`${path}Error`] = message; 
-          }) 
-          this.setState(err)
-      }
-
-      console.log(response);
-  }
+    console.log(response);
+  };
 
   render() {
-    const { username, email, password, usernameError, emailError, passwordError } = this.state;
+    const {
+      username,
+      email,
+      password,
+      usernameError,
+      emailError,
+      passwordError
+    } = this.state;
 
     const errorList = [];
 
@@ -66,35 +80,51 @@ class Register extends Component {
 
     return (
       <Container text>
-        <Header as="h2">Register</Header>
-        <Input
-          error={!!usernameError}
-          placeholder="Username"
-          name="username"
-          onChange={this.onChange}
-          value={username}
-          fluid
-        />
-        <Input
-          placeholder="Email"
-          error={!!emailError}
-          name="email"
-          onChange={this.onChange}
-          value={email}
-          fluid
-        />
-        <Input
-          placeholder="Password"
-          name="password"
-          error={!!passwordError}
-          onChange={this.onChange}
-          value={password}
-          type="password"
-          fluid
-        />
-        <Button onClick={this.onSubmit}>Register</Button>
-        {usernameError || emailError || passwordError ? (
-          <Message error header="There was some errors with your submission" list={errorList} />
+        <Form>
+          <Header as="h2">Register</Header>
+
+          <Form.Field error={!!usernameError}>
+            <label>Username</label>
+            <Input
+              placeholder="Username"
+              name="username"
+              onChange={this.onChange}
+              value={username}
+              fluid
+            />
+          </Form.Field>
+
+          <Form.Field error={!!emailError}>
+            <label>Email</label>
+            <Input
+              placeholder="Email"
+              name="email"
+              onChange={this.onChange}
+              value={email}
+              fluid
+            />
+          </Form.Field>
+
+          <Form.Field error={!!passwordError}>
+            <label>Password</label>
+            <Input
+              placeholder="Password"
+              name="password"
+              onChange={this.onChange}
+              value={password}
+              type="password"
+              fluid
+            />
+          </Form.Field>
+
+          <Button onClick={this.onSubmit}>Register</Button>
+        </Form>
+        {errorList.length ? (
+          <Message
+            error
+            header="There was some errors with your submission"
+            list={errorList}
+          />
         ) : null}
       </Container>
     );
